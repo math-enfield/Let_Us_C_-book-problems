c57dc4bd634ca29ee1b6e70a017bb3e559e4686f
Byteknight <byteknight007@gmail.com>
2025-08-27 17:52:22 +0530

    further solve Problem 3.3

---
+++ Problem3-3.c
@@ -0,0 +1,24 @@
+/*an year in input, determine wheather is leap year or not*/
+#include <stdio.h>
+int main(){
+    int yr;
+    printf("enter the year: ");
+    scanf("%d",&yr);
+    if (yr%100==0){
+        if(yr%400==0){
+            printf("It's a leap year!");
+        }
+        else{
+            printf("It's not a leap year!");
+        }
+    }
+    else{
+        if (yr%4==0){
+        printf("It's a leap year!");
+        }
+        else{
+            printf("It's not a leap year!");
+        }
+    }
+    return 0;
+}
